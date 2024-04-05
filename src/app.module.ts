7c69c3d13b56b9645358076dc092daf13405016d
Nhat Anh Huynh <anhprono1998@gmail.com>
2024-04-05 16:59:50 +0700

    4 database bassic

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Event } from './event.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type:'mysql',
+    host:'localhost',
+    port: 3301,
+    username: 'root',
+    password: 'example',
+    database: 'nest-event',
+    entities: [Event],
+    synchronize: true
+  }),
+  TypeOrmModule.forFeature([Event])
+],
   controllers: [AppController,EventsController],
   providers: [AppService],
 })
